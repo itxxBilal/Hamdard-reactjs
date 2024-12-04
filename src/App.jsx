6f34d1938fb88b60b22f5295c6d8030ff7d48bd5
Bilal <g.bilal034@gmail.com>
2024-12-04 16:20:29 +0500

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import MediaGallery from './components/MediaGallery';
 import Footer from './components/Footer';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Donation from './components/Donation';
 import AdminDashboard from "./components/AdminDashboard"
